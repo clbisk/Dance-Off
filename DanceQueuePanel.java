c8ab59a5057e74f08261b43e94e4ce4ac706db8d
Chad Bisk <ckbisk@gmail.com>
2013-05-27 15:06:39 -0400

    panel.requestFocusInWindow();

--- DanceQueuePanel.java
+++ DanceQueuePanel.java
@@ -7,88 +7,92 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.util.ArrayList;
 
-
-/**panel to display dance queues for gameplay -- located to west of the gameplay panel*/
+/**
+ * panel to display dance queues for gameplay -- located to west of the gameplay
+ * panel
+ */
 public class DanceQueuePanel extends JPanel {
-	//private variables
-	/**an array of Arrow objects that will serve to store the coordinates of future arrows drawn on screen*/
+	// private variables
+	/**
+	 * an array of Arrow objects that will serve to store the coordinates of
+	 * future arrows drawn on screen
+	 */
 	private static ArrayList<Arrow> arrows;
 	private BufferedImage myImage;
 	private Graphics myBuffer;
 	private Timer timer;
-	/**variable to store the lowest x-coordinate of the box where keys are considered "on time"*/
+	/**
+	 * variable to store the lowest x-coordinate of the box where keys are
+	 * considered "on time"
+	 */
 	private int boxXMin = 50;
-	/**variable to store the highest x-coordinate of the box where keys are considered "on time"*/
+	/**
+	 * variable to store the highest x-coordinate of the box where keys are
+	 * considered "on time"
+	 */
 	private int boxXMax = 170;
-	/**variable to store the lowest y-coordinate of the box where keys are considered "on time"*/
+	/**
+	 * variable to store the lowest y-coordinate of the box where keys are
+	 * considered "on time"
+	 */
 	private int boxYMin = 0;
-	/**variable to store the highest y-coordinate of the box where keys are considered "on time"*/
+	/**
+	 * variable to store the highest y-coordinate of the box where keys are
+	 * considered "on time"
+	 */
 	private int boxYMax = 600;
-	/**constructor*/
+
+	/** constructor */
 	public DanceQueuePanel() {
-		//flow layout
-		//uses buffer to draw arrows based on queues in an array
+		// flow layout
+		// uses buffer to draw arrows based on queues in an array
 		setFocusable(true);
 		addKeyListener(new ArrowPressed());
-		setBorder(BorderFactory.createLineBorder(Color.black));
+		setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK, 3), "DanceQueue"));
 	}
-	
-	//methods
-	/**getter for private array arrows*/
+
+	// methods
+	/** getter for private array arrows */
 	private static Arrow[] getArrows() {
 		Arrow[] a = arrows.toArray(new Arrow[arrows.size()]);
 		return a;
 	}
-	/**getter for element of private array arrows*/
+
+	/** getter for element of private array arrows */
 	private static Arrow getArrow(int arrowPos) {
 		Arrow[] a = arrows.toArray(new Arrow[arrows.size()]);
 		return a[arrowPos];
 	}
-	/**listener to redraw panel according to timer*/
+
+	/** listener to redraw panel according to timer */
 	private class Listener implements ActionListener {
-  		/**redraws panel*/ 	
+		/** redraws panel */
 		public void actionPerformed(ActionEvent e) {
-			//clears buffer and move all arrows
-			//repaint();
+			// clears buffer and move all arrows
+			// repaint();
 		}
 	}
-// 	public boolean isFocusTraversable() {
-// 		return true;
-//    }
-	/**listener for right arrow key*/
-	//private class rightArrowPressed extends KeyAdapter {
-		/**handles scoring for right arrow dance queues*/
-		//public void keyPressed(KeyEvent e) {
-			//int key = e.getKeyCode();
-			//System.out.println("right arrow pressed");
-			//if (key == KeyEvent.VK_RIGHT) {
-				//checks if right arrow dance queue is being shown inside
-				//Arrow[] a = arrows.toArray(new Arrow[arrows.size()]);
-				//for (int i = 0; i < a.length; i ++) {
-					//if (a[i] instanceof RightArrow) {
-						//if ((a[i].getYPos()+100 > boxYMin && a[i].getYPos() < boxYMax) && (a[i].getXPos() > boxXMin && a[i].getXPos() < boxXMax)) {
-							//if so, remove right arrow image from panel and add points to score
-						//}
-					//}
-				//}
-			//}			
-		//}
-	//}
-	private class ArrowPressed implements KeyListener{
+
+	private class ArrowPressed implements KeyListener {
 		@Override
-       public void keyPressed(KeyEvent e){
-         if(e.getKeyCode()== KeyEvent.VK_RIGHT)
-            System.out.println("right"); 
-         if(e.getKeyCode()== KeyEvent.VK_LEFT)
-            System.out.println("left");
-         if(e.getKeyCode()== KeyEvent.VK_UP)
-            System.out.println("up");
-         if(e.getKeyCode()== KeyEvent.VK_DOWN)
-            System.out.println("down");
-      }
-       public void keyReleased(KeyEvent e){
-      }
-       public void keyTyped(KeyEvent e){
-      }
-   }
+		public void keyPressed(KeyEvent e) {
+			System.out.println(e.toString());
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+				System.out.println("right");
+			if (e.getKeyCode() == KeyEvent.VK_LEFT)
+				System.out.println("left");
+			if (e.getKeyCode() == KeyEvent.VK_UP)
+				System.out.println("up");
+			if (e.getKeyCode() == KeyEvent.VK_DOWN)
+				System.out.println("down");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
+	}
 }
--- Danceoff.java
+++ Danceoff.java
@@ -1,85 +1,101 @@
 //Pd 5 AccCS RoseS
 //Cecilia Bisk, Syona Satwah, Joanna Gerr
 
-   import javax.swing.*;
-   import java.awt.*;
-   import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-/**contains main function and other functions facilitating the transitioning of screens*/
-   public class Danceoff {
-   //private variables to store difficulty and song choice
-   /**
-   	integer representing the song chosen by user when prompted by the song
-   	choice panel; song A being represented by -1, song B being represented
-   	by 0, and song C being represented by 1
-   */
-      private static int song;
-   /**
-   	integer value representing the difficulty level chosen by user when
-   	prompted by the difficulty selection panel; easy difficulty being
-   	represented by 0, and hard difficulty being represented by 1
-   */
-      private static int difficulty;
-      private static JFrame frame = new JFrame("Dance-off!");
-   
-   //methods
-   /**main method to be run for Danceoff program*/
-      public static void main(String[] args) throws Exception {
-         frame.setSize(600,600);
-         frame.setLocation(400, 100);
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         displayTitleScreen();
-      }
-   /**displays the title screen panel*/
-      public static void displayTitleScreen() {
-      //accesses title screen layout from TitleScreen class
-         frame.setContentPane(new TitleScreen());
-         frame.setVisible(true);
-      }
-   /**displays instructions panel*/
-      public static void displayInstructions() {
-      //accesses instructions screen layout from InstructionScreen class
-         frame.setContentPane(new InstructionScreen());
-         frame.setVisible(true);
-      }
-   /**displays high scores panel*/
-      public static void displayHighScores() {
-      //accesses high scores screen layout from HighScoreScreen class
-         frame.setContentPane(new HighScoreScreen());
-         frame.setVisible(true);
-      }
-   /**displays song selection panel*/
-      public static void startGame() {
-      //accesses song selection screen layout from SongSelectionScreen class
-         frame.setContentPane(new SongSelectionScreen());
-         frame.setVisible(true);
-      }
-   /**setter for private variable song*/
-      public static void setSong(int tune) {
-         song = tune;
-      }
-   /**getter for private variable song*/
-      public static int getSong() {
-         return song;
-      }
-   /**displays difficulty selection panel*/
-      public static void difficultySelect() {
-      //accesses song selection screen layout from DifficultySelectionScreen class
-         frame.setContentPane(new DifficultySelectionScreen());
-         frame.setVisible(true);
-      }
-   /*setter for private variable difficulty**/
-      public static void setDifficulty(int level) {
-         difficulty = level;
-      }
-   /*getter for private variable difficulty**/
-      public static int getDifficulty() {
-         return difficulty;
-      }
-   /**displays panel for game play after selections are complete*/
-      public static void gamePlayStart() {
-      //accesses layout from GamePlayPanel class
-         frame.setContentPane(new DanceQueuePanel());
-         frame.setVisible(true);
-      }
-   }
+/**
+ * contains main function and other functions facilitating the transitioning of
+ * screens
+ */
+public class Danceoff {
+	// private variables to store difficulty and song choice
+	/**
+	 * integer representing the song chosen by user when prompted by the song
+	 * choice panel; song A being represented by -1, song B being represented by
+	 * 0, and song C being represented by 1
+	 */
+	private static int song;
+	/**
+	 * integer value representing the difficulty level chosen by user when
+	 * prompted by the difficulty selection panel; easy difficulty being
+	 * represented by 0, and hard difficulty being represented by 1
+	 */
+	private static int difficulty;
+	private static JFrame frame = new JFrame("Dance-off!");
+
+	// methods
+	/** main method to be run for Danceoff program */
+	public static void main(String[] args) throws Exception {
+		frame.setSize(600, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		displayTitleScreen();
+	}
+
+	/** displays the title screen panel */
+	public static void displayTitleScreen() {
+		// accesses title screen layout from TitleScreen class
+		frame.setContentPane(new TitleScreen());
+		frame.setVisible(true);
+	}
+
+	/** displays instructions panel */
+	public static void displayInstructions() {
+		// accesses instructions screen layout from InstructionScreen class
+		frame.setContentPane(new InstructionScreen());
+		frame.setVisible(true);
+	}
+
+	/** displays high scores panel */
+	public static void displayHighScores() {
+		// accesses high scores screen layout from HighScoreScreen class
+		frame.setContentPane(new HighScoreScreen());
+		frame.setVisible(true);
+	}
+
+	/** displays song selection panel */
+	public static void startGame() {
+		// accesses song selection screen layout from SongSelectionScreen class
+		frame.setContentPane(new SongSelectionScreen());
+		frame.setVisible(true);
+	}
+
+	/** setter for private variable song */
+	public static void setSong(int tune) {
+		song = tune;
+	}
+
+	/** getter for private variable song */
+	public static int getSong() {
+		return song;
+	}
+
+	/** displays difficulty selection panel */
+	public static void difficultySelect() {
+		// accesses song selection screen layout from DifficultySelectionScreen
+		// class
+		frame.setContentPane(new DifficultySelectionScreen());
+		frame.setVisible(true);
+	}
+
+	/** setter for private variable difficulty* */
+	public static void setDifficulty(int level) {
+		difficulty = level;
+	}
+
+	/** getter for private variable difficulty* */
+	public static int getDifficulty() {
+		return difficulty;
+	}
+
+	/** displays panel for game play after selections are complete */
+	public static void gamePlayStart() {
+		// accesses layout from GamePlayPanel class
+		DanceQueuePanel panel = new DanceQueuePanel();
+		frame.setContentPane(panel);
+		panel.requestFocusInWindow();
+		frame.setVisible(true);
+	}
+}
