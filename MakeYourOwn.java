ebd2fd13f0ca1e768c2b7ced684eb71832a3deb7
J Gerr <deflating@gmail.com>
2013-06-02 23:59:57 -0400

    outputting stuff
    outputs the arrows to be stuck into an array! woot

--- MakeYourOwn.java
+++ MakeYourOwn.java
@@ -88,21 +88,25 @@ public class MakeYourOwn extends JPanel {
 				//System.out.println("right");
 				dancerPanel.turnRight();
 				MakeYerOwn.right();
+				System.out.print("new RightArrow("+MakeYerOwn.time+"), ");
 			}
 			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 				//System.out.println("left");
 				dancerPanel.turnLeft();
 				MakeYerOwn.left();
+				System.out.print("new LeftArrow("+MakeYerOwn.time+"), ");
 			}
 			if (e.getKeyCode() == KeyEvent.VK_UP) {
 				//System.out.println("up");
 				dancerPanel.turnUp();
 				MakeYerOwn.up();
+				System.out.print("new UpArrow("+MakeYerOwn.time+"), ");
 			}
 			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 				//System.out.println("down");
 				dancerPanel.turnDown();
 				MakeYerOwn.down();
+				System.out.print("new DownArrow("+MakeYerOwn.time+"), ");
 			}
 		}
 
