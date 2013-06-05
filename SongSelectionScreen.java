34951591b4950083c9f5e44df391e9f86779e8d5
clbisk <clbisk@gmail.com>
2013-06-05 11:00:49 -0300

    Update SongSelectionScreen.java
    +buton

--- SongSelectionScreen.java
+++ SongSelectionScreen.java
@@ -28,6 +28,10 @@ public class SongSelectionScreen extends JPanel {
 		JPanel panel3= new JPanel();
 		panel3.setLayout(new BorderLayout(0, 20));
 		panel2.add(panel3, BorderLayout.CENTER);
+		
+		JPanel panel4= new JPanel();
+		panel4.setLayout(new BorderLayout(0, 20));
+		panel3.add(panel4, BorderLayout.CENTER);
 	
 		JPanel panelA = new JPanel();
 		panelA.setLayout(new FlowLayout());
@@ -44,6 +48,11 @@ public class SongSelectionScreen extends JPanel {
 		JPanel panel4A = new JPanel();
 		panel4A.setLayout(new FlowLayout());
 		panel.add(panel4A, BorderLayout.SOUTH);
+		
+		JPanel panel5A = new JPanel();
+		panel5A.setLayout(new FlowLayout());
+		panel4.add(panel5A, BorderLayout.NORTH);
+		
 	
 		JButton button1= new JButton("Song C (Bad Girl -- Instrumental)");
 		button1.addActionListener(new SongAListener());
@@ -57,6 +66,14 @@ public class SongSelectionScreen extends JPanel {
 		JButton button4= new JButton("Go Back");
 		button4.addActionListener(new BackListener());
 		button4.setPreferredSize(new Dimension(300, 30));
+		
+		if ("customdance.ser" != null){
+			JButton button5 = new JButton("Your Song");
+			button5.addActionListener(new SongDListener());
+			button5.setPrefferedSize(new Dimension(300, 30));
+			panel5A.add(button5, BorderLayout.NORTH);
+		}
+		
 		panel3A.add(button1, BorderLayout.NORTH);
 		panel2A.add(button2, BorderLayout.NORTH);
 		panelA.add(button3, BorderLayout.NORTH);
@@ -95,6 +112,15 @@ public class SongSelectionScreen extends JPanel {
 			Danceoff.setSong(1);
 			Danceoff.difficultySelect();
 
+		}
+	}
+	/** SongD Listener for Selection Screen*/
+	private class SongDListener implements ActionListener {
+		/**Calls method to show difficulty selection screen;
+		Passes int signifying the selected song (Song C)*/
+		public void actionPerformed(ActionEvent d) {
+			Danceoff.setSong(2);
+			Danceoff.gamePlayStart();
 		}
 	}
 	/** Back Listener for Selection Screen*/
